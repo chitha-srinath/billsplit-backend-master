e8cfc5a8022f947778bb6579b45e65ebec2fcf43
chitha_srinath <chithasaireddy@gmail.com>
2026-03-20 17:34:39 +0530

    Initial commit

---
+++ src/Controller/LoginController.ts
@@ -0,0 +1,241 @@
+import { Request, Response } from "express";
+import HandleResponse, { ApiResponse } from "../Utility/ResponseHandle";
+import LoginService, {
+  ISignInServiceInterface,
+} from "../Services/LoginService";
+import { IGetUserAuthInfoRequest } from "../Utility/RequestModifier";
+
+export interface ILoginControllerInterface {
+  login(req: Request, res: Response): Promise<any>;
+  register(req: Request, res: Response): Promise<any>;
+  getuser(req: Request, res: Response): Promise<any>;
+  logout(req: Request, res: Response): Promise<any>;
+  handleGoogleLogin(req: Request, res: Response): Promise<any>;
+}
+
+class LoginController implements ILoginControllerInterface {
+  constructor(private loginService: ISignInServiceInterface) {}
+
+  public async login(req: Request, res: Response): Promise<any> {
+    try {
+      const serviceInfo = await this.loginService.signIn(req.body);
+      let result;
+      if (serviceInfo?.message) {
+        let accessoptions = {
+          //domain: process.env.DOMAIN_URL!,
+          maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+          httpOnly: true, // The cookie only accessible by the web server
+          secure: true, // Set to true if your site is served via HTTPS
+          path: "/",
+          sameSite: "none", // Enforce strict SameSite mode
+        } as any;
+        let options = {
+          //domain: process.env.DOMAIN_URL!,
+          maxAge: 1000 * 60 * 60 * 24, // would expire after 15 minutes
+          httpOnly: true, // The cookie only accessible by the web server
+          secure: true, // Set to true if your site is served via HTTPS
+          path: "/",
+          sameSite: "none", // Enforce strict SameSite mode
+        } as any;
+
+        // Set cookie
+        res.cookie("access", serviceInfo.acessToken, accessoptions);
+        res.cookie("refresh", serviceInfo.refreshToken, options);
+        result = HandleResponse.handleResponse(
+          true,
+          200,
+          serviceInfo.message,
+          null
+        );
+      } else {
+        result = HandleResponse.handleResponse(
+          false,
+          400,
+          serviceInfo.error,
+          null
+        );
+      }
+      return res.send(result);
+    } catch (err) {
+      console.log("login error", err);
+      const result: ApiResponse = HandleResponse.handleResponse(
+        false,
+        500,
+        "Login failed",
+        null
+      );
+      return res.send(result);
+    }
+  }
+
+  public async register(req: Request, res: Response): Promise<any> {
+    try {
+      const serviceInfo = await this.loginService.signUp(req.body);
+      let result: ApiResponse;
+
+      if (serviceInfo?.message) {
+        result = HandleResponse.handleResponse(
+          true,
+          200,
+          serviceInfo.message,
+          null
+        );
+      } else {
+        result = HandleResponse.handleResponse(
+          false,
+          409,
+          serviceInfo.error,
+          null
+        );
+      }
+      return res.send(result);
+    } catch (err) {
+      const result: ApiResponse = HandleResponse.handleResponse(
+        false,
+        500,
+        "Register failed",
+        null
+      );
+      return res.send(result);
+    }
+  }
+
+  public async getuser(
+    req: IGetUserAuthInfoRequest,
+    res: Response
+  ): Promise<any> {
+    try {
+      console.log("-----user details-----", req?.userDetails);
+
+      const serviceInfo = await this.loginService.getuserInfo(req?.userDetails);
+
+      const result: ApiResponse = HandleResponse.handleResponse(
+        true,
+        200,
+        "user fetchied sucessfully",
+        serviceInfo
+      );
+      return res.send(result);
+    } catch (err) {
+      console.log("------err from fetch user details ----", err);
+      const result: ApiResponse = HandleResponse.handleResponse(
+        false,
+        500,
+        "fetch user failed",
+        null
+      );
+      return res.send(result);
+    }
+  }
+
+  public async logout(
+    req: IGetUserAuthInfoRequest,
+    res: Response
+  ): Promise<any> {
+    try {
+      const serviceInfo = await this.loginService.signOut(req.userDetails);
+      let result: ApiResponse;
+      console.log("---serviceInfo?.message---", serviceInfo?.message);
+      if (serviceInfo?.message) {
+        let accessoptions = {
+          //domain: process.env.DOMAIN_URL!,
+          //maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+          httpOnly: true, // The cookie only accessible by the web server
+          secure: true, // Set to true if your site is served via HTTPS
+          path: "/",
+          sameSite: "none", // Enforce strict SameSite mode
+        } as any;
+        let options = {
+          //  domain: process.env.DOMAIN_URL!,
+          //maxAge: 1000 * 60 * 60 * 24, // would expire after 15 minutes
+          httpOnly: true, // The cookie only accessible by the web server
+          secure: true, // Set to true if your site is served via HTTPS
+          path: "/",
+          sameSite: "none", // Enforce strict SameSite mode
+        } as any;
+        res.clearCookie("access", accessoptions);
+        res.clearCookie("refresh", options);
+        result = HandleResponse.handleResponse(
+          true,
+          200,
+          serviceInfo.message,
+          null
+        );
+      } else {
+        result = HandleResponse.handleResponse(
+          false,
+          400,
+          "couldn't signout user",
+          null
+        );
+      }
+      return res.send(result);
+    } catch (err) {
+      console.log(err);
+      const result: ApiResponse = HandleResponse.handleResponse(
+        false,
+        500,
+        "couldn't logout user",
+        null
+      );
+      return res.send(result);
+    }
+  }
+
+  public async handleGoogleLogin(req: Request, res: Response): Promise<any> {
+    try {
+      console.log(req.cookies);
+      const serviceInfo = await this.loginService.googleLogin(req.user);
+      let result: ApiResponse;
+
+      if (serviceInfo?.message) {
+        let accessoptions = {
+          //domain: process.env.DOMAIN_URL!,
+          maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+          httpOnly: true, // The cookie only accessible by the web server
+          secure: true, // Set to true if your site is served via HTTPS
+          path: "/",
+          sameSite: "none", // Enforce strict SameSite mode
+        } as any;
+        let options = {
+          //domain: process.env.DOMAIN_URL!,
+          maxAge: 1000 * 60 * 60 * 24, // would expire after 15 minutes
+          httpOnly: true, // The cookie only accessible by the web server
+          secure: true, // Set to true if your site is served via HTTPS
+          path: "/",
+          sameSite: "none", // Enforce strict SameSite mode
+        } as any;
+
+        // Set cookie
+        res.cookie("access", serviceInfo.acessToken, accessoptions);
+        res.cookie("refresh", serviceInfo.refreshToken, options);
+
+        // result = HandleResponse.handleResponse(
+        //   true,
+        //   200,
+        //   serviceInfo.message,
+        //   null
+        // );
+        return res.status(200).redirect("http://localhost:5173");
+      } else {
+        result = HandleResponse.handleResponse(
+          false,
+          400,
+          serviceInfo.error,
+          null
+        );
+        // return res.status(400).redirect("http://localhost:5173/login");
+      }
+    } catch (err) {
+      const result: ApiResponse = HandleResponse.handleResponse(
+        false,
+        500,
+        "Google login failed",
+        null
+      );
+      return res.send(result);
+    }
+  }
+}
+
+export default LoginController;
---
+++ src/Routes/loginRoutes.ts
@@ -0,0 +1,63 @@
+import { Router } from "express";
+import LoginController, {
+  ILoginControllerInterface,
+} from "../Controller/LoginController";
+import LoginService from "../Services/LoginService";
+import Authentication from "../Middlewares/Authentication";
+import passport from "../Passport/passportconfig";
+
+class LoginRoutes {
+  private readonly router: Router;
+
+  constructor(private loginController: ILoginControllerInterface) {
+    this.router = Router();
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes(): void {
+    this.router
+      .route("/login")
+      .post(this.loginController.login.bind(this.loginController));
+    this.router
+      .route("/register")
+      .post(this.loginController.register.bind(this.loginController));
+    this.router
+      .route("/getUserDetails")
+      .get(
+        Authentication,
+        this.loginController.getuser.bind(this.loginController)
+      );
+    this.router
+      .route("/signout")
+      .get(
+        Authentication,
+        this.loginController.logout.bind(this.loginController)
+      );
+
+    this.router.route("/google").get(
+      passport.authenticate("google", {
+        scope: ["profile", "email"],
+      }),
+      (_, res) => {
+        res.send("redirecting to google...");
+      }
+    );
+
+    this.router
+      .route("/google/callback")
+      .get(
+        passport.authenticate("google"),
+        this.loginController.handleGoogleLogin.bind(this.loginController)
+      );
+  }
+
+  public getRoutes(): Router {
+    return this.router;
+  }
+}
+
+// Initialize dependencies outside the class
+const loginService = new LoginService();
+const loginController = new LoginController(loginService);
+
+export default new LoginRoutes(loginController).getRoutes();
